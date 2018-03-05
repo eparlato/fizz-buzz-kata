1bf3cd12dfe2a90b18e8ac3c5a86471debc728e1
eparlato <eparlato82@gmail.com>
2018-03-05 21:50:49 +0100

    First test: I can print the content of a single-value array of numbers.

---
+++ src/js/fizz_buzz.js
@@ -0,0 +1,10 @@
+function FizzBuzz() {
+
+    this.run = function(numbers) {
+        var linesToPrint = [];
+
+        linesToPrint.push(numbers[0]);
+
+        return linesToPrint;
+    };
+}
--- test/js/fizz_buzz_spec.js
+++ test/js/fizz_buzz_spec.js
@@ -1,5 +1,15 @@
-describe('WHen I play the Fizz Buzz game', function() {
-    it('jasmine works', function() {
-        expect(true).toEqual(true);
+describe('When I run the game', function() {
+    var linesToPrint;
+    var fizzBuzz;
+    var numbers;
+    
+    it('I can send a list with one number and receive a list with the same number', function() {
+        numbers = [1];
+        
+        fizzBuzz = new FizzBuzz();
+
+        linesToPrint = fizzBuzz.run(numbers);
+
+        expect(linesToPrint[0]).toEqual(1);
     });
 });
